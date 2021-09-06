ee8d842904f437bbb6154b2ffa3173b8d6f26e79
misael-diaz <misael.diaz@upr.edu>
2021-09-06 12:44:00 -0400

    performs style changes (step-response) [MATLAB]

--- transient-response/examples/second-order-dynamic-systems/step.m
+++ transient-response/examples/second-order-dynamic-systems/step.m
@@ -1,13 +1,13 @@
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% %
-% Course: System Dynamics and Control                       ME 3030-21 WI19
-% Author: Prof. M Diaz-Maldonado                          Date: Dec/27/2019
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% %
+% System Dynamics and Control                             December 27, 2019
+% ME 3030-21 WI19
+%
+% Prof. M Diaz-Maldonado
 % Synopsis:
 % Reproduces the step response of example 7.8 from Kluever's textbook.
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% %
-clear;
-close all;
-clc;
+%
+clear
+close all
+clc
 
 % mechanical system parameters
 % moment of inertia
@@ -18,12 +18,12 @@ b = 1.6;
 k = 65;
 
 % magnitude of step input
-A = 2.5; 
+A = 2.5;
 
 % standard coefficients of the second-order system
 a1 = b/J; a0 = k/J; b0 = 1/J;
 
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% %
+
 % defines the second-order constants which specify a second-order system:
 
 % natural frequency
@@ -35,12 +35,12 @@ DR = a1 / (2*Wn);
 alpha = -DR * Wn;
 beta  = Wn * sqrt(1-DR^2);
 
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% %
+
 % calculates the transient response, y(t), Equation (7.72)
 t = linspace(0,2,500);
-y = b0 * A / Wn^2 *...
-    (1 - exp(alpha * t) .* (cos(beta*t) - alpha/beta * sin(beta*t)));
-plot(t,y,'-k'); hold on;
+y = (1 - exp(alpha * t) .* (cos(beta*t) - alpha/beta * sin(beta*t)));
+y = (b0 * A / Wn^2) * y;
+plot(t, y, '-k'); hold on;
 
 
 
@@ -48,7 +48,6 @@ plot(t,y,'-k'); hold on;
 xlabel('time, t, sec');
 ylabel('underdamped step response, y(t)');
 title('step response of an underdamped mechanical system');
-%title('step response of an underdamped second order system (\omega_n = 1 rad/s)');
 grid on;
 
 % exports figure to PNG format with 600 DPI

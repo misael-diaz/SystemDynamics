69275c0154c8d7e9db45c2594634f538cd4839aa
misael-diaz <misael.diaz@upr.edu>
2021-09-06 12:51:16 -0400

    performs additional style changes (step) [MATLAB]

--- transient-response/examples/second-order-dynamic-systems/step.m
+++ transient-response/examples/second-order-dynamic-systems/step.m
@@ -5,50 +5,47 @@
 % Synopsis:
 % Reproduces the step response of example 7.8 from Kluever's textbook.
 %
+
 clear
 close all
 clc
 
 % mechanical system parameters
-% moment of inertia
-J = 0.2;
-% friction coefficient
-b = 1.6;
-% stiffness
-k = 65;
+J = 0.2;			% moment of inertia
+b = 1.6;			% friction coefficient
+k = 65.;			% stiffness
 
 % magnitude of step input
 A = 2.5;
 
 % standard coefficients of the second-order system
-a1 = b/J; a0 = k/J; b0 = 1/J;
+a1 = b / J;	a0 = k / J;	b0 = 1 / J;
 
 
 % defines the second-order constants which specify a second-order system:
 
-% natural frequency
-Wn = sqrt(a0);
-% damping ratio
-DR = a1 / (2*Wn);
+Wn = sqrt(a0);			% natural frequency
+DR = a1 / (2 * Wn);		% damping ratio
 
 % real and imaginary parts of the roots of the characteristic equation
-alpha = -DR * Wn;
-beta  = Wn * sqrt(1-DR^2);
+alpha = -DR * Wn;		% real
+beta  = Wn * sqrt(1 - DR^2);	% imaginary
 
 
 % calculates the transient response, y(t), Equation (7.72)
-t = linspace(0,2,500);
-y = (1 - exp(alpha * t) .* (cos(beta*t) - alpha/beta * sin(beta*t)));
+t = linspace(0, 2, 512);
+y = (  1 - exp(alpha * t) .* ( cos(beta*t) - alpha/beta * sin(beta*t) )  );
 y = (b0 * A / Wn^2) * y;
-plot(t, y, '-k'); hold on;
+plot(t, y, '-k')
+hold on
 
 
 
 % specifies the figure labels, the title, and the legend
-xlabel('time, t, sec');
-ylabel('underdamped step response, y(t)');
-title('step response of an underdamped mechanical system');
-grid on;
+xlabel('time, t, sec')
+ylabel('underdamped step response, y(t)')
+title('step response of an underdamped mechanical system')
+grid on
 
 % exports figure to PNG format with 600 DPI
-print('step-response-mechanical-system.png','-r600','-dpng');
+print('step-response-mechanical-system.png','-r600','-dpng')

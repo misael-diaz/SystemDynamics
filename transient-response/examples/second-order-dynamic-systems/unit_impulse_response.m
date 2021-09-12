1303cbfecba47b9c90e549ebdd0f391edfbc924c
misael-diaz <misael.diaz@upr.edu>
2021-09-11 21:27:07 -0400

    adds GPLv3 notice and references [MATLAB]

--- transient-response/examples/second-order-dynamic-systems/unit_impulse_response.m
+++ transient-response/examples/second-order-dynamic-systems/unit_impulse_response.m
@@ -1,8 +1,21 @@
 % System Dynamics and Control                       	   January 22, 2020
 % ME 3030-21 WI19
 % Prof. M Diaz-Maldonado
+%
 % Synopsis:
-% Plots the response of a second order system to a unit impulse input.
+% Plots the response of an underdamped, second-order, dynamic system to a
+% unit-impulse (P = 1) input.
+%
+%
+% Copyright (c) 2021 Misael Diaz-Maldonado
+% This file is released under the GNU General Public License as published
+% by the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+%
+% References:
+% [0] CA Kluever, Dynamic Systems: Modeling, Simulation, and Control
+% [1] A Gilat, MATLAB: An Introduction with Applications, 6th edition
 
 clear
 close all
@@ -17,7 +30,7 @@ alpha = -DR * Wn;
 beta  = Wn * sqrt(1 - DR^2);
 
 % calculates the transient response, y(t)
-% Note: b0 = 1/Wn^2 and A=1.
+% Note: b0 = Wn^2 and P=1, Kluever's Equation (7.66)
 t = linspace(0, 30, 1024);
 y = beta * (1 + (alpha / beta)^2) * exp(alpha * t) .* sin(beta * t);
 plot(t, y, '-k', 'linewidth', 2); hold on
